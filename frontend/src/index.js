35b97f8ba1b72fd0edceebdfbcc6f0afd5c4bc51
MacFly <matteo.ventura1999@gmail.com>
2026-07-08 19:10:17 +0700

    Create the environment file and the index file for the frontend side

---
+++ frontend/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
